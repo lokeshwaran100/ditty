4eb283153c98bfcae875b9bb396d78aedce157fc
Adithya2310 <Adithyanandi099@gmail.com>
2023-09-24 19:42:13 +0530

    Added the connect functionality

--- app/app/layout.js
+++ app/app/layout.js
@@ -2,6 +2,11 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import NavBar from '@/components/NavBar'
 import SideBar from '@/components/SideBar'
+import dynamic from 'next/dynamic';
+// import WalletConnectionProvider from '@/context/WalletConnectionProvider'
+const WalletConnectionProvider=dynamic(()=>import("../context/WalletConnectionProvider"),{
+  ssr:false,
+});
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,11 +19,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <WalletConnectionProvider>
       <NavBar/>
       <div className='flex gap-1'>
         <SideBar/>
         {children}
       </div>
+      </WalletConnectionProvider>
       </body>
     </html>
   )
--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,6 +1,12 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
 import { logo } from '@/assets'
+import dynamic from 'next/dynamic';
+const WalletMultiButtonDynamic = dynamic(
+    async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+    { ssr: false }
+);
 
 const NavBar = () => {
   return (
@@ -9,7 +15,8 @@ const NavBar = () => {
         <div className="flex items-center  justify-between">
             <Image src={logo} alt="Your Logo" className="w-12 h-auto" />
             <h1 className='text-2xl font-bold '>Ditty</h1>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer">Connect</button>
+            {/* <button className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer">Connect</button> */}
+            <WalletMultiButtonDynamic startIcon={<button className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"/>}/>
         </div>
         </nav>
     </div>
